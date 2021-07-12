0f14de046bb4ed92f1b06b5854bbf3917af1312f
cui112 <cui@qqq.com>
2021-07-12 15:06:39 +0800

    lodash

---
+++ lodash/talangzhe005-lodash.js
@@ -0,0 +1,23 @@
+var talangzhe005 = (function(){
+  function chunk(array, size = 1){
+    var res = []
+    var a = []
+    var count = 0
+    for(var i = 0; i < array.length; i++){
+      
+      a.push(array[i])
+      count++
+      if(count == size){
+        res.push(a)
+        count = 0
+        a = []
+      }
+    }
+    if(a.length){
+      res.push(a)
+    }
+    
+    return res
+  }
+  
+})()
