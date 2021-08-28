f219f2731ebd3db4fc410eb489c8efad369f1457
cui112 <cui@qqq.com>
2021-08-28 17:13:59 +0800

    kkkk

--- lodash/talangzhe005-lodash.js
+++ lodash/talangzhe005-lodash.js
@@ -436,7 +436,7 @@ var talangzhe005 = (function(){
     let res = []
     for (let i = 0; i < array.length; i++) {
       if (Array.isArray(array[i])) {
-        for (k of array[i]) {
+        for (let k of array[i]) {
           res.push(k)
         }
       }else {
