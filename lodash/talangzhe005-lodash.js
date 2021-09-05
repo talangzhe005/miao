1b70a6afdc1cf3367320fe81fa7b1b4ca5d3f1c7
cui112 <cui@qqq.com>
2021-09-05 16:26:49 +0800

    fdsafsad

--- lodash/talangzhe005-lodash.js
+++ lodash/talangzhe005-lodash.js
@@ -1094,7 +1094,7 @@ var talangzhe005 = (function(){
 
   function flip(func) {
     return function(...args) {
-      return func(reverse(...args))
+      return func(...args.reverse())
     }
   }
 
